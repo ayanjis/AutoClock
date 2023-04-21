af9dd2bb3770af342e91872d3ffc54978ceb1739
ayanjis <ayancdas@gmail.com>
2023-04-21 11:51:35 -0600

    Update with realtime

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 const $ = id => document.getElementById(id)
 
-var d = new Date
-let h = d.getHours()
-let m = d.getMinutes()
-let s = d.getSeconds()
-let newTime = `${h} : ${m} : ${s}`
-console.log(newTime)
+function updateTime() {
+    let d = new Date
+    let h = d.getHours()
+    let m = d.getMinutes()
+    let s = d.getSeconds()
+    
+    if (h < 10) {
+        h = `0${m}` 
+    }
+    if (m < 10) {
+        m = `0${m}` 
+    }
+    if (s < 10) {
+        s = `0${s}` 
+    }
 
-console.log($('Hours').textContent = h)
-console.log($('Minutes').textContent = m)
-console.log($('Seconds').textContent = s)
+    // let t_str = h + ":" + m + " ";
+    // if (h > 11) {
+    //     t_str += "PM";
+    // } else {
+    //     t_str += "AM";
+    // }
+
+    // let newTime = `${h} : ${m} : ${s}`
+    // console.log(newTime) 
+
+    $('Hours').textContent = h
+    $('Minutes').textContent = m
+    $('Seconds').textContent = s
+
+}
+setInterval(updateTime, 1000);
